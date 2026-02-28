9f6be91c401e6e265db623a21f897d7e0609c203
Prashanth1916 <prashanth29032002@gmail.com>
2026-02-28 17:49:55 +0530

    zerodha frontend

---
+++ src/landing_page/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+function NotFound() {
+    return ( 
+         <div className='container p-5 mb-5'>
+        <div classsName='row text-center'>
+           
+            <h1 classNAme='mt-5'>404 Not Found!!</h1>
+            <p>Sorry, the page you are looking for does not exist
+            </p>
+        </div>
+       </div>
+    );
+}
+
+
+export default NotFound;
