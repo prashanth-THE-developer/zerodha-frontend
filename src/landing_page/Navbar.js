35c6ee9857e5d2efa6bc8b92c61ef9f2bd11f8b9
Prashanth1916 <prashanth29032002@gmail.com>
2026-02-28 18:58:59 +0530

    fixed zerodha logo svg

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
+
 function Navbar() {
   return (
     <nav
-      className="navbar navbar-expand-lg border-bottom"
+      class="navbar navbar-expand-lg border-bottom"
       style={{ backgroundColor: "#FFF" }}
     >
-      <div className="container p-2">
-        <a className="navbar-brand" href="#">
-          <span style={{fontSize: "1.5rem", fontWeight: "700", color: "#387ed1"}}>Zerodha</span>
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img
+            src="media/images/logo.svg"
+            style={{ width: "25%" }}
+            alt="Logo"
+          />
         </a>
         <button
-          className="navbar-toggler"
+          class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -18,25 +23,35 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <form className="d-flex" role="search">
-            <ul className="navbar-nav mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Signup</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">About</a>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Product</a>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Product
+                </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Pricing</a>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Support</a>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
               </li>
             </ul>
           </form>
@@ -45,4 +60,5 @@ function Navbar() {
     </nav>
   );
 }
+
 export default Navbar;
