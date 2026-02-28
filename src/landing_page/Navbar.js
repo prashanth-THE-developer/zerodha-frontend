9d284b1bcaeb4f349aafd59c2d141d31293c8bd2
Prashanth1916 <prashanth29032002@gmail.com>
2026-02-28 18:31:38 +0530

    fixed navbar logo

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,53 +1,63 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
 
 function Navbar() {
   return (
-    
-      <nav className="navbar navbar-expand-lg border-bottom" 
-      style={{backgroundColor: "#fff"}}>
-        <div className="container p-2">
-          <Link className="navbar-brand" to="/">
-            <img src="media/images/logo.svg" style={{width: "25%"}} alt="Logo" />
-            </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-           
-            <form className="d-flex" role="search">
-                <ul className="navbar-nav  mb-lg-0">   
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/signup">
-                 Signup
-                  </Link>
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img
+            src="media/images/logo.svg"
+            style={{ width: "25%" }}
+            alt="Logo"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/about">
-                 About
-                  </Link>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
               </li>
-               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/product">
-                 Product
-                  </Link>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Product
+                </a>
               </li>
-               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/pricing">
-                 Pricing
-                  </Link>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
               </li>
-               <li className="nav-item">
-               <Link className="nav-link active" aria-current="page" to="/support">
-                 Support
-                  </Link>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
               </li>
-          </ul>
-            </form>
-          </div>
+            </ul>
+          </form>
         </div>
-      </nav>
-  
+      </div>
+    </nav>
   );
 }
 
